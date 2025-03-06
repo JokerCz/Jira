01cf37529fffa2b9ed6eb9509d3e6ba1d87b5721
JokerCz <9662882+jokercz1107@user.noreply.gitee.com>
2025-03-06 23:20:30 +0800

    add jest and lint etc

---
+++ src/utils/index.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export const cleanObject = (object: { [key: string]: unknown }) => {
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    if (value === undefined || value === null || value === "") {
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = <V>(value: V, delay?: number): V => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
